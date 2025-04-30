989c9dc096de01282dd9d20787a25e995a0c1511
opv1 <pas7273@gmail.com>
2025-04-30 22:54:31 +0300

    step2

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -3,15 +3,15 @@ import jwt from 'jsonwebtoken';
 import UnauthorizedError from '../errors/unauthorizedError';
 
 const authHandler = (req: Request, res: Response, next: NextFunction) => {
-  const { authorization } = req.headers;
+  const { cookie } = req.headers;
 
-  if (!authorization || !authorization.startsWith('Bearer ')) {
+  if (!cookie || !cookie?.startsWith('jwt=')) {
     next(new UnauthorizedError('Необходима авторизация'));
     return;
   }
 
   let payload;
-  const token = authorization.replace('Bearer ', '');
+  const token = cookie.replace('jwt=', '');
 
   try {
     payload = jwt.verify(token, JSON.stringify(process.env.JWT_SECRET));
